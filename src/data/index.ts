3d23faa1850f3507ffe67b9705347696dae93f12
qinshixixing <qinshixixing@icloud.com>
2022-03-18 11:50:46 +0800

    初始化模版

---
+++ src/data/index.ts
@@ -0,0 +1,4 @@
+export const testData: Data.TestData = {
+  name: 'test',
+  text: 'test'
+};
---
+++ type/data.d.ts
@@ -0,0 +1,6 @@
+declare namespace Data {
+  interface TestData {
+    name: string;
+    text: string;
+  }
+}
